a6f17875db8fede8d8ee85959be9b189d1c7286c
geoblo <geoblo@naver.com>
2023-10-19 21:27:02 +0900

    up

--- 11_React/my-app/src/chapter9/9.4/MainPage.jsx
+++ 11_React/my-app/src/chapter9/9.4/MainPage.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const styles = {
   button: {
     height: 40,
@@ -13,10 +15,41 @@ const styles = {
   }
 };
 
+function WarningBanner(props) {
+  console.log(props.warn);
+  if (!props.warn) {
+    return null;
+  }
+
+  return (
+    <div style={styles.warning}>Warning!</div>
+  );
+}
+
+function DangerBanner() {
+  return (
+    <div style={styles.warning}>Danger!</div>
+  );
+}
+
 function MainPage() {
+  const [showWarning, setShowWarning] = useState(false);
+
+  const handleToggle = () => {
+    // setShowWarning(!showWarning);
+    setShowWarning(prevState => !prevState);
+  };
+
   return (
     <>
-    
+      <WarningBanner warn={showWarning} />
+
+      {/* 보통의 경우 아래와 같이 조건부 렌더링으로 처리하는 것도 가능 */}
+      {showWarning && <DangerBanner />}
+
+      <button style={styles.button} onClick={handleToggle}>
+        {showWarning ? '감추기' : '보이기'}
+      </button>
     </>
   );
 }
