dce362062f77ad6c7fd1cef7b893498311c7a81a
geoblo <geoblo@naver.com>
2023-10-31 20:24:41 +0900

    업

---
+++ 11_React/my-news/src/components/NewsItem.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+const NewsItemBlock = styled.div`
+  display: flex;
+
+  .thumbnail {
+    margin-right: 1rem;
+    img {
+      display: block;
+      width: 160px;
+      height: 100px;
+      object-fit: cover;
+    }
+  }
+
+  .contents {
+    h2 {
+      margin: 0;
+      a {
+        color: black;
+      }
+    }
+
+    p {
+      margin: 0;
+      line-height: 1.5;
+      margin-top: 0.5rem;
+      white-space: normal;
+    }
+  }
+
+  & + & {
+    margin-top: 3rem;
+  }
+`;
+
+// 각 뉴스 정보를 보여주는 컴포넌트
+function NewsItem(props) {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default NewsItem;
