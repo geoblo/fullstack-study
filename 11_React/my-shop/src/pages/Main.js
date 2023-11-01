5275bb8a7a7de57f18ae66aa0ebd20eedfbb4ec7
geoblo <geoblo@naver.com>
2023-11-01 20:17:16 +0900

    필기

--- 11_React/my-shop/src/pages/Main.js
+++ 11_React/my-shop/src/pages/Main.js
@@ -51,3 +51,34 @@ function Main(props) {
 }
 
 export default Main;
+
+// 가짜(Fake) API 서버 만들기
+// 실무와 비슷한 느낌으로 하기 위해 가짜(Fake) API 서버를 만들거임
+
+// 1. json-server (혼자 CRUD 연습하기 좋음)
+// 이 도구를 사용하면 json 파일 하나만 있으면 로컬에 연습용 서버를 쉽게 구성 할 수 있음
+// (즉, 사용하려는 컴퓨터에서 매번 로컬 서버를 띄워야 함)
+
+// json-server 사용법
+// ./src/data.json 이라는 파일을 작성
+// npx json-server ./src/data.json --port 4000
+// 또는 
+// npm i -g json-server
+// json-server --watch ./src/data.json --port 4000
+
+// 더 자세한 사용법 참고
+// https://github.com/typicode/json-server
+// https://redux-advanced.vlpt.us/3/01.html
+
+// 2. My JSON Server (Read만 가능)
+// 이 서비스를 사용하면 GitHub와 연동하여 연습용 서버를 쉽게 구성 할 수 있음
+
+// My JSON Server 사용법
+// GitHub에 저장소 생성(<your-username>/<your-repo>)
+// db.json파일 만들기
+// 서버에 액세스하려면 https://my-json-server.typicode.com/<your-username>/<your-repo>를 방문
+
+// 사용 예
+// https://my-json-server.typicode.com/geoblo/db-shop
+// https://my-json-server.typicode.com/geoblo/db-shop/products
+// https://my-json-server.typicode.com/geoblo/db-shop/products/1
