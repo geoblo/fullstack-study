d44a5441fd1d7bd64bba4e02df42a460963c44dd
geoblo <geoblo@naver.com>
2023-11-01 16:28:50 +0900

    필기

--- 11_React/my-app/src/index.js
+++ 11_React/my-app/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from "react-redux";
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -48,6 +50,8 @@ import EffectSummary from './chapter7/7.3/ex2/EffectSummary';
 import EffectContainer from './chapter7/7.3/ex2/EffectContainer';
 import TimerContainer from './chapter7/7.3/ex3/TimerContainer';
 import ApiRequest from './chapter17/ApiRequest';
+import ReduxTestApp from './chapter18/ReduxTestApp';
+import { store } from './chapter18/app/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -142,10 +146,15 @@ root.render(
   // <SimpleRouter />
   
   // 17장 예제
-  <ApiRequest />
+  // <ApiRequest />
 
-
-
+  // 18장 예제
+  // 2. 리액트에 Redux Store 제공하기
+  // ReduxTestApp 컴포넌트와 그 하위 자식들은 Redux Store에 접근 가능
+  // Redux Store에 저장된 state들을 마음대로 꺼내 쓸 수 있음
+  <Provider store={store}>
+    <ReduxTestApp />
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
