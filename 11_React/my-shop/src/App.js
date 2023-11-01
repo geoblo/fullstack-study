891ba7b260a185afd4485ff1734772ceee064bdf
geoblo <geoblo@naver.com>
2023-11-01 19:04:41 +0900

    업

---
+++ 11_React/my-shop/src/App.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+// Bootstrap
+// 레이아웃을 복사 붙여넣기 식으로 편하게 개발 가능한 css, js 라이브러리
+// 리액트 용이 따로 있는데 나는 기존 것이 더 익숙하다 싶으면 기존 부트스트랩을 써도 무관
+// https://react-bootstrap.netlify.app/
+
+// 패키지 설치 및 StrictMode 제거
+// npm install react-bootstrap bootstrap styled-components react-router-dom @reduxjs/toolkit react-redux axios
