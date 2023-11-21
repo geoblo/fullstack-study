35f7e57b69c409096584b53fc44983c06874ba66
geoblo <geoblo@naver.com>
2023-11-21 12:12:12 +0900

    up

--- 12_Node.js/ch3/3.5/url.js
+++ 12_Node.js/ch3/3.5/url.js
@@ -5,4 +5,6 @@ console.log(url);
 const { URL } = url;
 
 // 참고로 URL은 노드 내장 객체이기도 해서 require 안하고 바로 사용 가능
-new URL('http://www.gilbut.co.kr/book/bookList.aspx?sercate1=001001000#anchor');
+const myURL = new URL('http://www.gilbut.co.kr/book/bookList.aspx?sercate1=001001000#anchor');
+console.log('new URL():', myURL); // URL 객체
+console.log('url.format():', url.format(myURL)); // URL 객체를 다시 문자열로 변환
