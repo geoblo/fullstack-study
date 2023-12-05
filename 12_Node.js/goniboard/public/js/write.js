875a8d1880ad6524e01afd66c8de307706cd04cd
geoblo <geoblo@naver.com>
2023-12-05 17:30:46 +0900

    up

--- 12_Node.js/goniboard/public/js/write.js
+++ 12_Node.js/goniboard/public/js/write.js
@@ -7,7 +7,13 @@ document.querySelector('#write-form').addEventListener('submit', async (e) => {
   //   return alert('제목을 입력하세요');
   // }
   try {
-    const result = await axios.post('/post/write', { title, content });
+    const formData = new FormData(); // multipart/form-data 타입으로 보냄
+    formData.append('title', title);
+    formData.append('content', content);
+    formData.append('img', e.target.img.files[0]);
+
+    // const result = await axios.post('/post/write', { title, content });
+    const result = await axios.post('/post/write', formData);
     console.log(result.data);
 
     if (!result.data.flag) {
