981c69e497a5e066d28180d7784cdab8b03a00c3
geoblo <geoblo@naver.com>
2024-01-09 12:49:03 +0900

    up

---
+++ 13_Next.js/goniboard/app/list/ListItem.js
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import DetailButton from "./DetailButton";
+
+export default function ListItem(props) {
+  const { post } = props;
+
+  return (
+    <div className="list-item">
+      {/* 페이지를 이동하는 방법(1) - Link 컴포넌트 */}
+      {/* Quiz: Link 컴포넌트를 사용하여 '/detail/글id'로 이동 */}
+      <h4>
+        <Link href={`/detail/${post._id}`}>{post.title}</Link>
+      </h4>
+
+      {/* 페이지를 이동하는 방법(2) - useRouter */}
+      <DetailButton postId={post._id.toString()} />
+
+      <p>{post.content}</p>
+    </div>
+  );
+}
+
+// 상세 페이지 만들기
+// 1) 글 제목 누르면 상세 페이지로 이동
+// 2) DB에서 해당 게시글 가져와서 보여주기
+// => 이때 상세 페이지 URL은? /detail/123
+// => /app/detail 폴더 안에 여러 폴더 만들면 비효율적
+// => React: URL 파라미터, Express: 라우트 파라미터
+// => Next.js: Dynamic Routes 사용 -> 파일 또는 폴더 이름을 대괄호로 묶어 생성 []
